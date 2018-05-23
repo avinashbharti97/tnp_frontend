29726824e6f6b05731cd8d90f2770c73cc55b409
Avinash Bharti <avinashbharti97@gmail.com>
2018-05-23 18:17:26 +0530

    v2.0 updated

--- script.js
+++ script.js
@@ -1,102 +1,121 @@
-  
-$(document).ready(function(){
-$("#scrollButton").click(function(event) {
-  // alert("h");
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      console.log(target);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
+  $(document).ready(function() {
+      $("#scrollButton").click(function(event) {
+          // alert("h");
+          // On-page links
+          if (
+              location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+              location.hostname == this.hostname
+          ) {
+              // Figure out element to scroll to
+              var target = $(this.hash);
+              console.log(target);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              // Does a scroll target exist?
+              if (target.length) {
+                  // Only prevent default if animation is actually gonna happen
+                  event.preventDefault();
+                  $('html, body').animate({
+                      scrollTop: target.offset().top
+                  }, 1000, function() {
+                      // Callback after animation
+                      // Must change focus!
+                      var $target = $(target);
+                      $target.focus();
+                      if ($target.is(":focus")) { // Checking if the target was focused
+                          return false;
+                      } else {
+                          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                          $target.focus(); // Set focus again
+                      };
+                  });
+              }
+          }
+      });
 
 
   });
 
 
   $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+      $('.navbar-collapse').collapse('hide');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 48
-  });
+  // $('body').scrollspy({
+  //     target: '#mainNav',
+  //     offset: 48
+  // });
 
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  });
-
-
+  // // Collapse the navbar when page is scrolled
+  // $(window).scroll(function() {
+  //     if ($("#mainNav").offset().top > 100) {
+  //         $("#mainNav").addClass("navbar-shrink");
+  //     } else {
+  //         $("#mainNav").removeClass("navbar-shrink");
+  //     }
+  // });
 
 
-   // Scroll reveal calls
-  window.sr = ScrollReveal();
-  sr.reveal('.sr-icons', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 200);
-  sr.reveal('.sr-button', {
-    duration: 1000,
-    delay: 200
-  });
-  sr.reveal('.sr-contact', {
-    duration: 600,
-    scale: 0.3,
-    distance: '0px'
-  }, 300);
+
+
+  // Scroll reveal calls
+  // window.sr = ScrollReveal();
+  // sr.reveal('.sr-icons', {
+  //     duration: 600,
+  //     scale: 0.3,
+  //     distance: '0px'
+  // }, 200);
+  // sr.reveal('.sr-button', {
+  //     duration: 1000,
+  //     delay: 200
+  // });
+  // sr.reveal('.sr-contact', {
+  //     duration: 600,
+  //     scale: 0.3,
+  //     distance: '0px'
+  // }, 300);
 
   // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+  // $('.popup-gallery').magnificPopup({
+  //     delegate: 'a',
+  //     type: 'image',
+  //     tLoading: 'Loading image #%curr%...',
+  //     mainClass: 'mfp-img-mobile',
+  //     gallery: {
+  //         enabled: true,
+  //         navigateByImgClick: true,
+  //         preload: [0, 1]
+  //     },
+  //     image: {
+  //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+  //     }
+
+  // })(jQuery); // End of use strict
+
+
+
+
+  //navbar
+  console.log("hey");
+  $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+          $('.navbar').addClass('color-change color-change-2');
+      } else {
+          $('.navbar').removeClass('color-change color-change-2');
+      }
   });
 
-})(jQuery); // End of use strict
+  //carasoul
+  $('.carousel').carousel({
+      interval: 1000
+  })
 
 
-
-
-//button
-
+  function toggleIcon(e) {
+      $(e.target)
+          .prev('.panel-heading')
+          .find(".more-less")
+          .toggleClass('glyphicon-plus glyphicon-minus');
+  }
+  $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+  $('.panel-group').on('shown.bs.collapse', toggleIcon);
